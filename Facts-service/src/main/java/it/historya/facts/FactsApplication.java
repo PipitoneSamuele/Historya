20c313dcac6ae9c7d0dd8d4a3aefd8b2002bc657
PipitoneSamuele <samuele.pipitone@gmail.com>
2024-12-08 15:53:40 +0100

    created Facts-service

---
+++ Facts-service/src/main/java/it/historya/facts/FactsApplication.java
@@ -0,0 +1,13 @@
+package it.historya.facts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactsApplication.class, args);
+	}
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ## Scope
 Personal project about history. The main objective is to learn tools and apply some ideas to a potentially infinite project, meanwhile it would be good to learn some history.
+
+## Microservices
+
+### Quickstart
+- Spring Initializr: 
